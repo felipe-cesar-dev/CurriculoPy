22b793d82accd1ca6d8f76336a6cf6f5ab8fe1c2
Felipe César Porto Oliveira <136658021+felipe-cesar-dev@users.noreply.github.com>
2025-04-02 15:01:00 -0300

    Editando View

---
+++ View/Sessoes.py
@@ -0,0 +1,13 @@
+class Sessoes:
+    def __init__(self):
+        self.__dadosPessoais = None
+        self.__infosCtt = None
+        self.__infosPess = None
+        self.__redes = None
+        self.__cursos = None
+        self.__conhecimentos = None
+        self.__exp = None
+        self.__formAcademica = None
+        self.__sobreMim = None
+
+    def criarSessoes(self, camada):
--- View/Tela.py
+++ View/Tela.py
@@ -5,46 +5,43 @@ class Tela:
         self.tk = tk.Tk()
         self.tk.geometry("1280x760")
         self.tk.title("Crie seu currículo")
-
         self.camada1 = tk.Canvas(self.tk, bg='#0E003F')
         self.camada1.pack(fill=tk.BOTH, expand=True)
-
         self.camada2 = tk.Canvas(self.camada1, width=1156, height=732, bg='#283EB8', highlightthickness=0)
         self.camada2.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-
         self.criarSessoes()
         self.criarLabels()
 
     def criarLabels(self):
-        label1 = self.Labels('Dados Pessoais')
+        label1 = self.Labels('Dados Pessoais', self.camada2)
         label1.place(relx=0.124, rely=0.035, anchor=tk.CENTER)
 
-        label2 = self.Labels('Informações de Contato')
+        label2 = self.Labels('Informações de Contato', self.camada2)
         label2.place(relx=0.374, rely=0.035, anchor=tk.CENTER)
 
-        label3 = self.Labels('Informações Pessoais')
+        label3 = self.Labels('Informações Pessoais', self.camada2)
         label3.place(relx=0.624, rely=0.035, anchor=tk.CENTER)
 
-        label4 = self.Labels('Redes Sociais')
+        label4 = self.Labels('Redes Sociais', self.camada2)
         label4.place(relx=0.874, rely=0.035, anchor=tk.CENTER)
 
-        label5 = self.Labels('Cursos')
+        label5 = self.Labels('Cursos', self.camada2)
         label5.place(relx=0.1095, rely=0.41, anchor=tk.CENTER)
 
-        label6 = self.Labels('Conhecimentos')
+        label6 = self.Labels('Conhecimentos', self.camada2)
         label6.place(relx=0.305, rely=0.41, anchor=tk.CENTER)
 
-        label7 = self.Labels('Experiências')
+        label7 = self.Labels('Experiências', self.camada2)
         label7.place(relx=0.5, rely=0.41, anchor=tk.CENTER)
 
-        label8 = self.Labels('Formação Acadêmica')
+        label8 = self.Labels('Formação Acadêmica', self.camada2)
         label8.place(relx=0.695, rely=0.41, anchor=tk.CENTER)
 
-        label9 = self.Labels('Sobre Mim')
+        label9 = self.Labels('Sobre Mim', self.camada2)
         label9.place(relx=0.89, rely=0.41, anchor=tk.CENTER)
 
-    def Labels(self, texto):
-        label = tk.Label(self.camada2, text=texto, bg='#283EB8', font=('Arial', 16), fg='white')
+    def Labels(self, texto, camada):
+        label = tk.Label(camada, text=texto, bg='#283EB8', font=('Times', 14), fg='white')
         return label
 
     def criarSessoes(self):
