ddd857bf80171e586c944767ac88585c4a25454d
Felipe César Porto Oliveira <136658021+felipe-cesar-dev@users.noreply.github.com>
2025-04-07 15:03:43 -0300

    reorganizando layout

--- View/Inputs.py
+++ View/Inputs.py
@@ -9,60 +9,67 @@ class Inputs(InputsABC):
     def __init__(self, labels: LabelsAbstrata):
         super().__init__()
         self.__labels = labels
+        self.a = 0.17
+        self.b = 0.43
+        self.c = 0.69
+        self.d = 0.37
+        self.y1 = 0.06
+        self.y2 = 0.307
+        self.y3 = 0.578
 
     def input(self, camada):
         input = tk.Entry(camada, bg='light gray', font=("Arial", 14), width=15)
         return input
 
     def criarInputsDados(self, camada):
-        self.__labels.labelsInputs('Nome: ', camada, 0.14, 0.20, CENTER)
-        self.__labels.labelsInputs('Profissão: ', camada, 0.18, 0.50, CENTER)
+        self.__labels.labelsInputs('Nome: ', camada, 0.14, self.y1, CENTER)
+        self.__labels.labelsInputs('Profissão: ', camada, 0.18, self.y2, CENTER)
 
         nome = self.input(camada)
-        nome.place(relx= 0.37, rely= 0.35, anchor=CENTER)
+        nome.place(relx= self.d, rely= self.a, anchor=CENTER)
 
         profissao = self.input(camada)
-        profissao.place(relx= 0.37, rely= 0.65, anchor=CENTER)
+        profissao.place(relx= self.d, rely= self.b, anchor=CENTER)
 
     def criarInputsContato(self, camada):
-        self.__labels.labelsInputs('Celular: ', camada, 0.14, 0.12, CENTER)
-        self.__labels.labelsInputs('E-mail: ', camada, 0.14, 0.42, CENTER)
-        self.__labels.labelsInputs('Endereço: ', camada, 0.20, 0.72, CENTER)
+        self.__labels.labelsInputs('Celular: ', camada, 0.14, self.y1, CENTER)
+        self.__labels.labelsInputs('E-mail: ', camada, 0.14, self.y2, CENTER)
+        self.__labels.labelsInputs('Endereço: ', camada, 0.19, self.y3, CENTER)
 
         celular = self.input(camada)
-        celular.place(relx= 0.37, rely= 0.27, anchor=CENTER)
+        celular.place(relx= self.d, rely= self.a, anchor=CENTER)
 
         email = self.input(camada)
-        email.place(relx= 0.37, rely= 0.57, anchor=CENTER)
+        email.place(relx= self.d, rely= self.b, anchor=CENTER)
 
         endereco = self.input(camada)
-        endereco.place(relx= 0.37, rely= 0.87, anchor=CENTER)
+        endereco.place(relx= self.d, rely= self.c, anchor=CENTER)
 
     def criarInputsPessoais(self, camada):
-        self.__labels.labelsInputs('Data de Nascimento: ', camada, 0.33, 0.12, CENTER)
-        self.__labels.labelsInputs('Estado Cívil: ', camada, 0.21, 0.42, CENTER)
-        self.__labels.labelsInputs('Nacionalidade: ', camada, 0.25, 0.72, CENTER)
+        self.__labels.labelsInputs('Data de Nascimento: ', camada, 0.33, self.y1, CENTER)
+        self.__labels.labelsInputs('Estado Cívil: ', camada, 0.21, self.y2, CENTER)
+        self.__labels.labelsInputs('Nacionalidade: ', camada, 0.25, self.y3, CENTER)
 
         nasc = self.input(camada)
-        nasc.place(relx= 0.37, rely= 0.27, anchor=CENTER)
+        nasc.place(relx= self.d, rely= self.a, anchor=CENTER)
 
         eCivil = self.input(camada)
-        eCivil.place(relx= 0.37, rely= 0.57, anchor=CENTER)
+        eCivil.place(relx= self.d, rely= self.b, anchor=CENTER)
 
         nacionalidade = self.input(camada)
-        nacionalidade.place(relx= 0.37, rely= 0.87, anchor=CENTER)
+        nacionalidade.place(relx= self.d, rely= self.c, anchor=CENTER)
 
     def criarInputsRedes(self, camada):
-        self.__labels.labelsInputs('Facebook: ', camada, 0.19, 0.12, CENTER)
-        self.__labels.labelsInputs('Instagram: ', camada, 0.19, 0.42, CENTER)
-        self.__labels.labelsInputs('Linkedin: ', camada, 0.17, 0.72, CENTER)
+        self.__labels.labelsInputs('Facebook: ', camada, 0.19, self.y1, CENTER)
+        self.__labels.labelsInputs('Instagram: ', camada, 0.19, self.y2, CENTER)
+        self.__labels.labelsInputs('Linkedin: ', camada, 0.17, self.y3, CENTER)
 
         face = self.input(camada)
-        face.place(relx= 0.37, rely= 0.27, anchor=CENTER)
+        face.place(relx= self.d, rely= self.a, anchor=CENTER)
 
         insta = self.input(camada)
-        insta.place(relx= 0.37, rely= 0.57, anchor=CENTER)
+        insta.place(relx= self.d, rely= self.b, anchor=CENTER)
 
         linkedin = self.input(camada)
-        linkedin.place(relx= 0.37, rely= 0.87, anchor=CENTER)
+        linkedin.place(relx= self.d, rely= self.c, anchor=CENTER)
     
