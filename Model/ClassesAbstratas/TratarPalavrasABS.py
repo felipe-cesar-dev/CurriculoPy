c82cd734a796e5f5fe4fe5fee7db901ed00230c4
Felipe César Porto Oliveira <136658021+felipe-cesar-dev@users.noreply.github.com>
2025-04-21 20:22:16 -0300

    a

--- Model/ClassesAbstratas/TratarPalavrasABS.py
+++
@@ -1,9 +0,0 @@
-from abc import abstractmethod, ABC
-
-class ControleLengthABS(ABC):
-    def __init__(self):
-        pass
-
-    @abstractmethod
-    def verificarNome(self, palavra):
-        pass
